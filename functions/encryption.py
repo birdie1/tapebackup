34bf307c3a53a98a82834b7fee8becaf077fb316
Jonas Vogel <jonas@vogel.xyz>
2020-03-21 16:02:10 +0100

    Add threading to encrypt function

--- functions/encryption.py
+++ functions/encryption.py
@@ -2,6 +2,9 @@ import logging
 import subprocess
 import os
 import time
+import threading
+from lib.database import Database
+
 
 logger = logging.getLogger()
 
@@ -13,12 +16,15 @@ class Encryption:
         self.tools = tools
         self.local_files = local
         self.interrupted = False
+        self.active_threads = []
 
     def set_interrupted(self):
         self.interrupted = True
 
-    def encrypt_single_file(self, id, filepath, filename_enc):
-        self.database.update_filename_enc(filename_enc, id)
+    def encrypt_single_file_thread(self, threadnr, id, filepath, filename_enc):
+        thread_db = Database(self.config)
+
+        thread_db.update_filename_enc(filename_enc, id)
 
         time_started = time.time()
 
@@ -45,7 +51,7 @@ class Encryption:
 
             filesize = os.path.getsize(os.path.abspath('{}/{}'.format(self.config['local-enc-dir'], filename_enc)))
             encrypted_date = int(time.time())
-            self.database.update_file_after_encrypt(filesize, encrypted_date, md5, id)
+            thread_db.update_file_after_encrypt(filesize, encrypted_date, md5, id)
 
             if not self.local_files:
                 time_started = time.time()
@@ -56,6 +62,8 @@ class Encryption:
             logger.warning("encrypt file failed, file: {} error: {}".format(id, openssl.stderr.readlines()))
             logger.debug(
                 "Execution Time: Encrypt file with openssl: {} seconds".format(time.time() - time_started))
+
+        self.active_threads.remove(threadnr)
 
 
     def encrypt(self):
@@ -68,19 +76,35 @@ class Encryption:
                 break
 
             for file in files:
-                logger.info("Processing: id: {}, filename: {}".format(file[0], file[1]))
+                for i in range(0, self.config['threads']):
+                    if i not in self.active_threads:
+                        next_thread = i
+                        break
+
+                logger.info("Starting Thread #{}, processing: id: {}, filename: {}".format(next_thread, file[0], file[1]))
 
                 filename_enc = self.tools.create_filename_encrypted()
                 while self.database.filename_encrypted_already_used(filename_enc):
                     logger.warning("Filename ({}) encrypted already exists, creating new one!".format(filename_enc))
                     filename_enc = self.tools.create_filename_encrypted()
 
-                self.encrypt_single_file(file[0], file[2], filename_enc)
+                self.active_threads.append(next_thread)
+                x = threading.Thread(target=self.encrypt_single_file_thread,
+                                     args=(next_thread, file[0], file[2], filename_enc,),
+                                     daemon=True)
+                x.start()
+
+                while threading.active_count() > self.config['threads']:
+                    time.sleep(10)
 
                 if self.interrupted:
+                    while threading.active_count() > 1:
+                        time.sleep(1)
                     break
 
             if self.interrupted:
+                while threading.active_count() > 1:
+                    time.sleep(1)
                 break
 
         ## encrypt
